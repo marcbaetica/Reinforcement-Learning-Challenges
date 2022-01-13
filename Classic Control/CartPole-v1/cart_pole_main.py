af54008512ad5767c115c3627ae8c5020685d9b6
Marc Baetica <marc.baetica@gmail.com>
2022-01-12 22:47:13 -0500

    Solved carpole RL challenge.

--- Classic Control/CartPole-v1/cart_pole_main.py
+++ Classic Control/CartPole-v1/cart_pole_main.py
@@ -1,8 +1,19 @@
 import gym
+from stable_baselines3 import A2C
 
 
 env = gym.make('CartPole-v1')
 episodes_total = 5
+model = A2C('MlpPolicy', env, verbose=1)
+model.learn(total_timesteps=100000)
+
+# Save the agent
+model.save("a2c_cartpole")
+del model  # delete trained model to demonstrate loading
+
+# Load the trained agent
+model = A2C.load("a2c_cartpole")
+
 
 for episode in range(episodes_total):
     observation = env.reset()
@@ -10,7 +21,8 @@ for episode in range(episodes_total):
     score = 0
     while not done:
         env.render()
-        action = env.action_space.sample()  # or given a custom model, action = policy(observation)
+        # action = env.action_space.sample()  # or given a custom model, action = policy(observation)
+        action, _state = model.predict(observation, deterministic=True)
         observation, reward, done, info = env.step(action)
         score += reward
     print(f'Episode {episode} score: {score}')
